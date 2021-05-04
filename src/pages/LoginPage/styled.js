f4177c9eb44e9ad31259edc1394f58480278d2c6
Beatriz Lisboa <lmourabia@gmail.com>
2021-05-04 17:12:10 -0300

    modificação da login page

--- src/pages/LoginPage/styled.js
+++ src/pages/LoginPage/styled.js
@@ -1,1 +1,20 @@
 import styled from 'styled-components'
+
+export const ScreenContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100vw;
+    margin-top: 20vh;
+`
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const SignUpContainer = styled.div`
+    width: 80vw;
+    max-width: 400px;
+    `
