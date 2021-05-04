2d5504d11bb19a3ec2d4ec2b89e3f56266b4f816
joaovitorcostaa <joao.vitoramc@gmail.com>
2021-05-04 15:04:06 -0300

    criando signup

--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,11 +1,109 @@
-import React from 'react'
+import React from "react";
+import {BASE_URL} from "../../constants/urls"
+import logo from "../../assets/logo-future-eats-invert.png";
+import { useHistory } from "react-router";
+import {goToEditAdressPage} from "../../routes/coordinator"
+import {
+    DivContainer,
+    Header, 
+    StyledInput,
+    StyledButton,
+    Form
+} from "./styled"
+import axios from "axios";
+import {useForm} from "../../hooks/useForm"
 
-const SignUpPage =()=> {
-    return (
-        <div>
-             <h1>SignUpPage</h1>
-        </div>
-    )
-}
+const SignUpPage = () => {
+  const history = useHistory();
 
-export default SignUpPage
+    const InitialState = {
+        name: "",
+        email: "",
+        cpf: "",
+        password: ""
+    }
+
+    const [form, setForm, handleForm, resetForm] = useForm(InitialState)
+
+  const signUp = () => {
+      axios.post(`${BASE_URL}signup`, form
+      ).then((res) => {
+          window.localStorage.setItem("token", res.data.token)
+          resetForm()
+          goToEditAdressPage()
+      }).catch((err) => {
+          console.log(err.response.data.message)
+          resetForm()
+      })
+  }
+
+  const handleClick = () => {
+    signUp()
+  };
+
+  return (
+    <DivContainer>
+      <Header>
+        <button onClick={() => goToLoginPage(history)}>voltar</button>
+      </Header>
+
+      <img src={logo}/>
+
+      <p>Cadastrar</p>
+
+        <Form>
+        <StyledInput
+          required
+          name="name"
+          label="Nome"
+          placeholder="Nome e sobrenome"
+          variant="outlined"
+          value={form.name}
+          onChange={handleForm}
+          type="text"
+        />
+        <StyledInput
+          required
+          name="email"
+          label="E-mail"
+          placeholder="email@email.com"
+          variant="outlined"
+          value={form.email}
+          onChange={handleForm}
+          type="text"
+        />
+        <StyledInput
+          required
+          name="cpf"
+          label="CPF"
+          placeholder="000.000.000-00"
+          variant="outlined"
+          value={form.cpf}
+          onChange={handleForm}
+          type="text"
+        />
+        <StyledInput
+          required
+          name="password"
+          label="Senha"
+          placeholder="Mínimo 6 caracteres"
+          variant="outlined"
+          value={form.password}
+          onChange={handleForm}
+          type="password"
+        />
+        <StyledInput
+          required
+          label="Confirmar senha"
+          placeholder="Confirme a senha anterior"
+          variant="outlined"
+          type="password"
+        />
+        <StyledButton onClick={() => handleClick()} variant="contained" color="primary"> Cadastrar </StyledButton>
+        </Form>
+
+    </DivContainer>
+  );
+};
+
+export default SignUpPage;
