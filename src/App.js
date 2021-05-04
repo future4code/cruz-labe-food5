0a35f10192c8e907693802b9dc9b21b7da30be35
Thiago Jatobá <tmjatob@gmail.com>
2021-05-04 11:37:42 -0300

    [feat] criação do GlobalState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Router from './routes/Router'
-
+import GlobalState from './global/GlobalState'
 
 function App() {
   return (
-    <div >
+    <GlobalState >
       <Router/>
-    </div>
+    </GlobalState>
   );
 }
 
