827bfe53d819f59cc692cafe703dddf89f6ad9b2
Guilherme Henrique Mota Alfredo <guilhermehmota12@gmail.com>
2021-05-05 16:41:34 -0300

    ajustes no profilePage, editProfile e editAddress

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,16 +1,15 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { BASE_URL } from '../../constants/urls'
 import axios from 'axios'
-import EditIcon from '@material-ui/icons/Edit';
-import { AddressDiv, ContainerProfile, ProfileDiv, Button } from './styled';
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
+import { AddressDiv, ContainerProfile, ProfileDiv, Button, HistoryContainer, AddressTitle } from './styled';
 import { goToEditAddressPage, goToEditProfilePage } from '../../routes/coordinator';
 import { useHistory } from 'react-router';
 
-const ProfilePage =()=> {
+const ProfilePage = () => {
     const history = useHistory()
     const [profile, setProfile] = useState({})
     // const [address, setAddress] = useState({})
-    // eslint-disable-next-line no-unused-vars
     // const [orderHistory, setOrderHistory] = useState({})
 
     useEffect(() => {
@@ -19,66 +18,69 @@ const ProfilePage =()=> {
         // getOrderHistory()
     }, [])
 
-const getProfile =()=> {
-    axios.get(`${BASE_URL}profile`, {
-        headers: {
-            auth: window.localStorage.getItem("token")
-        }
-    })
-    .then((res)=>{
-        setProfile(res.data.user)
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
-}
+    const getProfile = () => {
+        axios.get(`${BASE_URL}profile`, {
+            headers: {
+                auth: window.localStorage.getItem("token")
+            }
+        })
+            .then((res) => {
+                setProfile(res.data.user)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
 
-// const getFullAddress =()=> {
-//     axios.get(`${BASE_URL}profile/address`, {
-//         headers: {
-//             auth: window.localStorage.getItem("token")
-//         }
-//     })
-//     .then((res)=>{
-//         // setAddress(res.data.user.adress)
-//         console.log(res)
-//     })
-//     .catch((err)=>{
-//         console.log(err.response.data.message)
-//     })
-// }
+    // const getFullAddress =()=> {
+    //     axios.get(`${BASE_URL}profile/address`, {
+    //         headers: {
+    //             auth: window.localStorage.getItem("token")
+    //         }
+    //     })
+    //     .then((res)=>{
+    //         // setAddress(res.data.user.adress)
+    //         console.log(res)
+    //     })
+    //     .catch((err)=>{
+    //         console.log(err.response.data.message)
+    //     })
+    // }
 
-// const getOrderHistory =()=> {
-//     axios.get(`${BASE_URL}orders/history`, {
-//         headers: {
-//             auth: window.localStorage.getItem("token")
-//         }
-//     })
-//     .then((res)=>{
-//         setOrderHistory(res.data.order)
-//     })
-//     .catch((err)=>{
-//         console.log(err)
-//     })
-// }
+    // const getOrderHistory =()=> {
+    //     axios.get(`${BASE_URL}orders/history`, {
+    //         headers: {
+    //             auth: window.localStorage.getItem("token")
+    //         }
+    //     })
+    //     .then((res)=>{
+    //         setOrderHistory(res.data.order)
+    //     })
+    //     .catch((err)=>{
+    //         console.log(err)
+    //     })
+    // }
     return (
         <ContainerProfile>
             <h1>Meu perfil</h1>
             <ProfileDiv>
                 <div>
-                <p>{profile.name}</p>
-                <p>{profile.email}</p>
-                <p>{profile.cpf}</p>
+                    <p>{profile.name}</p>
+                    <p>{profile.email}</p>
+                    <p>{profile.cpf}</p>
                 </div>
-                <Button onClick={() => goToEditProfilePage(history)}><EditIcon/></Button>
+                <Button onClick={() => goToEditProfilePage(history)}><EditOutlinedIcon /></Button>
             </ProfileDiv>
             <AddressDiv>
                 <div>
-                {profile.address}
+                    <AddressTitle>Endereço cadastrado</AddressTitle>
+                    {profile.address}
                 </div>
-                <Button onClick={() => goToEditAddressPage(history)}><EditIcon/></Button>
+                <Button onClick={() => goToEditAddressPage(history)}><EditOutlinedIcon /></Button>
             </AddressDiv>
-            <h3>Histórico de pedidos</h3>
+            <HistoryContainer>
+                <h3>Histórico de pedidos</h3>
+            </HistoryContainer>
         </ContainerProfile>
     )
 }
