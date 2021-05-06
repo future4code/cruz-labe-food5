0da9f625e1fd6cf5cb01d6da98eef138f12d0812
Guilherme Henrique Mota Alfredo <guilhermehmota12@gmail.com>
2021-05-06 12:34:46 -0300

    layout profile, editDetails e edidAddress

--- src/pages/ProfilePage/styled.js
+++ src/pages/ProfilePage/styled.js
@@ -6,6 +6,17 @@ export const ContainerProfile = styled.div`
     justify-content: center;
     align-items: center;
 `
+export const Header = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+width: 100%;
+height: 4rem;
+margin: 0 0 1.5rem;
+-webkit-backdrop-filter: blur(10px);
+backdrop-filter: blur(10px);
+box-shadow: 0 0.5px 0 0 rgba(0, 0, 0, 0.25);  
+`;
 
 export const ProfileDiv = styled.div`
     display:flex;
@@ -18,14 +29,24 @@ export const ProfileDiv = styled.div`
     justify-content: space-between;   
 `
 export const AddressDiv = styled.div`
-    width: 20.5rem;
-    height: 1.125rem;
+    width: 22.5rem;
+    height: 100%;
     margin: 1rem 1rem 0.5rem;
     font-size: 1rem;
     letter-spacing: -0.39px;
     display: flex;
     align-items: center;
     justify-content: space-between;
+    background-color: #eeeeee;
+`
+export const DivInfoAddress = styled.div`
+    margin: 0 1rem;
+    display: flex;
+    flex-direction: column;
+`
+export const DivButton = styled.div`
+display: flex; 
+justify-content: space-between;
 `
 export const Button = styled.div`
     cursor: pointer;
@@ -33,8 +54,12 @@ export const Button = styled.div`
 export const HistoryContainer = styled.div`
     display: flex;
     flex-direction: column;
-    margin-top: 15px;
+    margin-top: 20px;
 `
 export const AddressTitle = styled.p`
 color: #b8b8b8;
+`
+export const DivTitle = styled.div`
+    border-bottom: 1px solid black;
+    width: 20.5rem;
 `
