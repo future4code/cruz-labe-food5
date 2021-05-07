1adb872449fad5909cc75bba3e514ce5488405b9
Guilherme Henrique Mota Alfredo <guilhermehmota12@gmail.com>
2021-05-07 20:07:15 -0300

    histórico de pedidos

--- src/pages/ProfilePage/styled.js
+++ src/pages/ProfilePage/styled.js
@@ -46,3 +46,11 @@ export const DivTitle = styled.div`
     border-bottom: 1px solid black;
     width: 20.5rem;
 `
+export const CardOrder = styled.div`
+  width: 20.5rem;
+  height: 6.375rem;
+  margin: 0.438rem 0 0;
+  padding: 1rem;
+  border-radius: 8px;
+  border: solid 1px var(--greyish);
+`
